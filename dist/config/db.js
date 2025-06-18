0217a9080d61a4f273ac8b8c04fce2cdc9b39c39
suulayman5 <ajahsuleiman@gmail.com>
2025-06-18 13:12:06 +0100

    fix again

---
+++ dist/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+const MONGO_URI = process.env.DATABASE_URL;
+if (!MONGO_URI) {
+    console.log('MONGO_URI not defined in environment variables');
+}
+export async function connectDb() {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log('Connected to MongoDB ====>>>>>>', MONGO_URI);
+    }
+    catch (err) {
+        console.error('Failed to connect to MongoDB======>>>>>>>', err);
+        process.exit(1);
+    }
+}
